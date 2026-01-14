a0ac8ddab5ef78ae804fcb4213563791dcf129db
jordansobased <alfinjordandion@gmail.com>
2026-01-14 21:26:52 +0800

    menghapus jwt.php karena salah folder

--- auth/jwt.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Firebase\JWT\JWT;
-use Firebase\JWT\Key;
-
-$secret_key = "THIS_IS_A_VERY_LONG_RANDOM_SECRET_KEY_1234567890!@#$%^&*()AN";
-$issuer = "localhost";
-$audience = "localhost";
-$issued_at = time();
-$expiration_time = $issued_at + (60 * 60); // 1 jam
