b75695d632f4eb4f0e90de888c855b37b6af35f1
jordansobased <alfinjordandion@gmail.com>
2026-01-14 21:28:10 +0800

    Menambahkan api login dan autentifikasi jwt

--- auth/login.php
+++ auth/login.php
@@ -1,2 +1,43 @@
 <?php
-// login endpoint
+header("Content-Type: application/json");
+include_once "../config/database.php";
+include_once "../config/jwt.php";
+require "../vendor/autoload.php";
+require __DIR__ . '/../vendor/autoload.php';
+use Firebase\JWT\JWT;
+
+$data = json_decode(file_get_contents("php://input"));
+
+$db = new Database();
+$conn = $db->connect();
+
+$query = "SELECT * FROM users WHERE email = :email";
+$stmt = $conn->prepare($query);
+$stmt->bindParam(":email", $data->email);
+$stmt->execute();
+
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if ($user && password_verify($data->password, $user['password'])) {
+    $payload = [
+        "iss" => $issuer,
+        "aud" => $audience,
+        "iat" => $issued_at,
+        "exp" => $expiration_time,
+        "data" => [
+            "id" => $user['id'],
+            "nama" => $user['nama'],
+            "role" => $user['role']
+        ]
+    ];
+
+    $jwt = JWT::encode($payload, $secret_key, 'HS256');
+
+    echo json_encode([
+        "status" => true,
+        "token" => $jwt
+    ]);
+} else {
+    http_response_code(401);
+    echo json_encode(["message" => "Login gagal"]);
+}
