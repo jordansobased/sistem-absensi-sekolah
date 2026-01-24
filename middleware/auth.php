84ee182430f76b1ac1f3de33cfa99c9fed811922
jordansobased <alfinjordandion@gmail.com>
2026-01-24 15:58:23 +0800

    menambah middleware jwt

---
+++ middleware/auth.php
@@ -0,0 +1,29 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+include_once "../config/jwt.php";
+
+$headers = getallheaders();
+
+if (!isset($headers['Authorization'])) {
+    http_response_code(401);
+    echo json_encode(["message" => "Token tidak ditemukan"]);
+    exit;
+}
+
+$token = str_replace("Bearer ", "", $headers['Authorization']);
+
+try {
+    $decoded = JWT::decode($token, new Key($secret_key, 'HS256'));
+    $userData = $decoded->data; // id, nama, role
+} catch (Exception $e) {
+    http_response_code(401);
+    echo json_encode(["message" => "Token tidak valid"]);
+    exit;
+}
