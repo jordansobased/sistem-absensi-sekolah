c1e748f9fcb388e4993797379aaa0c3734e76029
jordansobased <alfinjordandion@gmail.com>
2026-01-26 02:05:43 +0800

    memperbaiki redudansi dan memberi komentar

--- users/read.php
+++ users/read.php
@@ -1,39 +1,16 @@
 <?php
 header("Content-Type: application/json");
+include_once "../middleware/auth.php";
 include_once "../config/database.php";
-include_once "../config/jwt.php";
-require "../vendor/autoload.php";
 
-use Firebase\JWT\JWT;
-use Firebase\JWT\Key;
-
-/* ========== CEK TOKEN ========== */
-$headers = getallheaders();
-
-if (!isset($headers['Authorization'])) {
-    http_response_code(401);
-    echo json_encode(["message" => "Token tidak ada"]);
+//cek role harus admin
+if ($userData->role !== 'admin') {
+    http_response_code(403);
+    echo json_encode(["message" => "Hanya admin"]);
     exit;
 }
 
-$token = str_replace("Bearer ", "", $headers['Authorization']);
-
-try {
-    $decoded = JWT::decode($token, new Key($secret_key, 'HS256'));
-
-    if ($decoded->data->role !== 'admin') {
-        http_response_code(403);
-        echo json_encode(["message" => "Akses ditolak"]);
-        exit;
-    }
-
-} catch (Exception $e) {
-    http_response_code(401);
-    echo json_encode(["message" => "Token tidak valid"]);
-    exit;
-}
-
-/* ========== AMBIL DATA USERS ========== */
+//ambil data users
 $db = new Database();
 $conn = $db->connect();
 
